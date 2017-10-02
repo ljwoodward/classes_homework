3f69f882d45517bcf5f6fbbcc6d22cdce3d8ff61
Laurence <ljwoodward.code@gmail.com>
2017-10-02 21:30:04 +0100

    second library test passed

--- library.rb
+++ library.rb
@@ -5,29 +5,39 @@ class Library
       {
         title: "lord_of_the_rings",
         rental_details: {
-         student_name: "Jeff",
-         date: "01/12/16"
+          student_name: "Jeff",
+          date: "01/12/16"
         }
       },
       {
         title: "the_catcher_in_the_rye",
         rental_details: {
-         student_name: "Duncan",
-         date: "01/01/17"
+          student_name: "Duncan",
+          date: "01/01/17"
         }
       },
       {
         title: "a_pale_view_of_hills",
         rental_details: {
-         student_name: "Rita",
-         date: "11/03/17"
+          student_name: "Rita",
+          date: "11/03/17"
         }
       }
     ]
   end
 
   def list_books
-      return @books
+    return @books
+  end
+
+  def book_details(name)
+    for book in @books
+      if book[:title] == name
+        return book
+      else return nil
+
+      end
+    end
   end
 
 end
--- specs/library_specs.rb
+++ specs/library_specs.rb
@@ -14,25 +14,36 @@ class TestLibrary < MiniTest::Test
       {
         title: "lord_of_the_rings",
         rental_details: {
-         student_name: "Jeff",
-         date: "01/12/16"
+          student_name: "Jeff",
+          date: "01/12/16"
         }
       },
       {
         title: "the_catcher_in_the_rye",
         rental_details: {
-         student_name: "Duncan",
-         date: "01/01/17"
+          student_name: "Duncan",
+          date: "01/01/17"
         }
       },
       {
         title: "a_pale_view_of_hills",
         rental_details: {
-         student_name: "Rita",
-         date: "11/03/17"
+          student_name: "Rita",
+          date: "11/03/17"
         }
       }
-    ])
-  end
+      ])
+    end
 
-end
+    def test_book_details
+      assert_equal(@library1.book_details("lord_of_the_rings"), {
+        title: "lord_of_the_rings",
+        rental_details: {
+          student_name: "Jeff",
+          date: "01/12/16"
+        }
+        })
+      
+    end
+
+  end
