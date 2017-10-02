47c1f30b14b33f9592d51cb2dac01fe8575e7ff7
Laurence <ljwoodward.code@gmail.com>
2017-10-02 16:03:36 +0100

    initial commit

---
+++ codeclan.rb
@@ -0,0 +1,12 @@
+class CodeClan
+
+  def initialize(input_name, input_cohort)
+    @name = input_name
+    @cohort = input_cohort
+  end
+
+def get_name
+  return @name
+end
+
+end
---
+++ specs/codeclan_specs.rb
@@ -0,0 +1,17 @@
+require "minitest/autorun"
+require "minitest/rg"
+require_relative "../codeclan.rb"
+
+class TestCodeClan < MiniTest::Test
+  @codeclan
+
+  def setup
+    @codeclan = CodeClan.new("Laurence", 16)
+  end
+
+def test_get_name
+  assert_equal(@codeclan.get_name, "Laurence")
+end
+
+
+end
